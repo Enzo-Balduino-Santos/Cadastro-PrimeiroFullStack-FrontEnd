8e7cd4b19e6bff550150bbc3d460d225a99b4c44
Enzo <161968684+Enzo-Balduino-Santos@users.noreply.github.com>
2025-10-23 13:42:41 -0300

    atualizações sobre o código

---
+++ src/persons.js
@@ -0,0 +1,28 @@
+const persons = [
+    {
+        name: "Guilherme Carvalho",
+        nickname: "whopperbatatafrita",
+        age: 16,
+        email: "carrossel@gmail.com"
+    },
+    {
+        name: "Victor Fustte",
+        nickname: "vg",
+        age: 12,
+        email: "vg_fustte@gmail.com"
+    },
+    {
+        name: "Sophia Leal",
+        nickname: "Garota confusa",
+        age: 20,
+        email: "sophialeal@gmail.com"
+    },
+    {
+        name: "Bomba",
+        nickname: "Bombão",
+        age: 27,
+        email: "bombinha@gmail.com"
+    },
+]
+
+export { persons }
